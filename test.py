4be95fa20290c240bafe62ef56f24de8072c9cef
ihydrad <86055286+ihydrad@users.noreply.github.com>
2023-12-23 00:33:19 +0500

    -

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 from keeloq import Keeloq
 
-key = 0x87318dcce381bd59
 mail = 0x55318DCCE381BD55
+key = 0x87318DCCE381BD59
+data = 0xE381BD59
 
 pager = Keeloq(key)
-print(hex(pager.decrypt(mail)))
+print("Data: " + hex(data))
+mail = pager.build_mail(pager.get_button_code(mail), pager.get_serial(mail), pager.encript(data))
+dec = pager.decrypt(mail)
+print("mail:", hex(mail))
+print("Decrypted: " + hex(dec))
+
